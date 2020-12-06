32843d21201caf83309715db5335b6c61dda7797
speier <kalman.speier@gmail.com>
2020-12-06 20:55:22 +0100

    pubkit append method

--- pubkit.go
+++ pubkit.go
@@ -1,11 +1,14 @@
 package pubkit
 
 import (
+	"encoding/base64"
 	"errors"
 
 	"github.com/speier/pubkit/internal/x25519"
 	"github.com/speier/pubkit/pkg/envelope"
 )
+
+var b64 = base64.RawStdEncoding.Strict()
 
 func GenerateKeys() ([]byte, []byte, error) {
 	pubkey, prvkey, err := x25519.GenerateKeys()
@@ -40,4 +43,28 @@ func Open(envelope *envelope.Envelope, prvkey []byte) ([]byte, error) {
 	}
 
 	return res, nil
+}
+
+func Append(envelope *envelope.Envelope, prvkey []byte, pubkey ...[]byte) (*envelope.Envelope, error) {
+	if envelope == nil {
+		return nil, errors.New("envelope is nil, must be specified")
+	}
+	if len(pubkey) == 0 {
+		return nil, errors.New("one or more public key must be specified")
+	}
+
+	data, err := Open(envelope, prvkey)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, rcpt := range envelope.Recipients {
+		rpk, err := b64.DecodeString(rcpt.PubKey)
+		if err != nil {
+			return nil, err
+		}
+		pubkey = append(pubkey, []byte(rpk))
+	}
+
+	return Seal(data, pubkey...)
 }
--- pubkit_test.go
+++ pubkit_test.go
@@ -28,6 +28,35 @@ func TestSealOpen(t *testing.T) {
 	}
 }
 
+func TestAppend(t *testing.T) {
+	// generate public/private key pairs
+	aPub, aPrv := mustGenKeys(t)
+	bPub, bPrv := mustGenKeys(t)
+
+	// encrypt for 'a'
+	want := []byte("hello")
+	secret, err := Seal(want, aPub)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// append 'b' pub key
+	modsecret, err := Append(secret, aPrv, bPub)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// decrypt for 'b' with private key
+	doc, err := Open(modsecret, bPrv)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if bytes.Compare(doc, want) != 0 {
+		t.Errorf("got %s, want %s", doc, want)
+	}
+}
+
 func mustGenKeys(t *testing.T) ([]byte, []byte) {
 	pub, prv, err := GenerateKeys()
 	if err != nil {
